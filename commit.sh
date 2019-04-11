e2ed3951f1ef336e288b00820d24f1631373d835
ChenXiaoSong12321 <448216252@qq.com>
2019-04-11 15:29:13 +0800

    add scss 2019.04.11 提交

--- commit.sh
+++ commit.sh
@@ -9,7 +9,7 @@ echo "执行提交"
 # git config --global user.name "ChenXiaoSong12321"
 git add .
 ls_date=`date +%Y.%m.%d`
-git commit -m "提交 $ls_date $1"
+git commit -m "$1 $ls_date 提交"
 git config --global credential.helper store
 git pull origin $branch
 git push origin $branch
---
+++ src/core/mixins/index.js
@@ -0,0 +1,80 @@
+function copyProperties(target, source) {
+  for (const key of Reflect.ownKeys(source)) {
+    if (key !== 'constructor' &&
+      key !== 'prototype' &&
+      key !== 'name'
+    ) {
+      const desc = Object.getOwnPropertyDescriptor(source, key)
+      Object.defineProperty(target, key, desc)
+    }
+  }
+}
+function compose(...funcs) {
+  if (funcs.length === 0) return arg => arg
+  if (funcs.length === 1) return funcs[0]
+  const last = funcs[funcs.length - 1]
+  const rest = funcs.slice(0, -1)
+  return (...args) => rest.reduceRight((composed, f) => f(...args), last(...args))
+}
+
+const LIFE_TIMES = ['beforeCreate', 'created', 'beforeMount', 'mounted', 'beforeDestroy', 'destroyed']
+
+export default function mix(...mixins) {
+  const lifetimes = {}
+  class Mix {
+    constructor() {
+      const properties = ['data', 'methods', 'computed', 'watch', 'props']
+      for (const Mixin of mixins) {
+        const mixin = new Mixin()
+        this.composeLifetimes(mixin) // 合并生命周期
+        properties.forEach(propertie => {
+          copyProperties(this[propertie], mixin[propertie] || {})
+        })
+      }
+    }
+    composeLifetimes(mixin) {
+      LIFE_TIMES.forEach(lifetime => {
+        // 循环合并生命周期
+        if (mixin[lifetime]) {
+          if (!lifetimes[lifetime])lifetimes[lifetime] = []
+          // 缓存生命周期
+          lifetimes[lifetime].push(mixin[lifetime])
+        }
+      })
+    }
+    beforeCreate(...args) {
+      // beforeCreate -- 实例未生成，无法调用 _lifetimes 方法
+      const methodsList = lifetimes['beforeCreate']
+      if (methodsList && methodsList.length > 0)compose(...methodsList.reverse().map(f => f.bind(this)))(...args)
+    }
+    created() {
+      this._lifetimes('created').apply(this, arguments)
+    }
+    beforeMount() {
+      this._lifetimes('beforeMount').apply(this, arguments)
+    }
+    mounted() {
+      this._lifetimes('mounted').apply(this, arguments)
+    }
+    beforeDestroy() {
+      this._lifetimes('beforeDestroy').apply(this, arguments)
+    }
+    destroyed() {
+      this._lifetimes('destroyed').apply(this, arguments)
+    }
+    data = {}
+    methods = {
+      _lifetimes(key) {
+        // 执行缓存生命周期
+        return function(...args) {
+          const methodsList = lifetimes[key]
+          if (methodsList && methodsList.length > 0)compose(...methodsList.reverse().map(f => f.bind(this)))(...args)
+        }
+      }
+    }
+    computed = {}
+    watch = {}
+  }
+
+  return Mix
+}
