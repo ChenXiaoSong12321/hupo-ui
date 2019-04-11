731e6dbf8265be2f2a55d8ec78c6455d9429bcae
ChenXiaoSong12321 <448216252@qq.com>
2019-04-11 13:49:12 +0800

    提交 2019.04.11 create popup

--- commit.sh
+++ commit.sh
@@ -7,7 +7,6 @@ branch=`git-branch-name`
 echo "执行提交"
 # git config --global user.email "448216252@qq.com"
 # git config --global user.name "ChenXiaoSong12321"
-doctoc .
 git add .
 ls_date=`date +%Y.%m.%d`
 git commit -m "提交 $ls_date $1"
