a6d9a441fc2a38a3e4745d5ae0a6746a909981db
ChenXiaoSong12321 <448216252@qq.com>
2019-04-17 17:47:50 +0800

    perfect tab push in 2019.04.17 by jerry

---
+++ src/assets/config/style.var.js
@@ -0,0 +1,13 @@
+export const primaryColor = '#dd392e'
+
+// 字体
+export const fontColorC1 = '#333333'
+export const fontColorC2 = '#666666'
+export const fontColorC3 = '#999999'
+
+// border
+export const borderColorD1 = '#d7d7d7'
+export const splitlineColorFgxC1 = '#e5e5e5'
+
+// background
+export const backgroundColorD2 = '#efefef'
