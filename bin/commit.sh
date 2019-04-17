b7393b24f232db0089619e82fddd2f67b170c167
ChenXiaoSong12321 <448216252@qq.com>
2019-04-17 13:53:13 +0800

    test commit origin github push in 2019.04.17

--- bin/commit.sh
+++ bin/commit.sh
@@ -9,13 +9,13 @@ branch=`git-branch-name`
 echo "执行提交"
 git config --global user.email "448216252@qq.com"
 git config --global user.name "ChenXiaoSong12321"
+git pull github $branch
+git pull origin $branch
 git add .
 ls_date=`date +%Y.%m.%d`
 git commit -m "$1 push in $ls_date"
 git config --global credential.helper store
-git pull origin $branch
-git pull github $branch
-git push origin $branch
 git push github $branch
+git push origin $branch
 
 echo "提交完成"
