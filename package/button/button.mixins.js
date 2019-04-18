1146178ece722b45786f8f16c0d35ce666a48668
ChenXiaoSong12321 <448216252@qq.com>
2019-04-18 14:12:09 +0800

    delete unuseful file push in 2019.04.18 by jerry

---
+++ package/button/button.mixins.js
@@ -0,0 +1,62 @@
+class HButtonMixins {
+  props = {
+    customClass: String,
+    customStyle: String,
+    radius: {
+      type: Boolean,
+      default: true
+    },
+    size: {
+      type: String,
+      default: 'large'
+    },
+    plain: {
+      type: Boolean,
+      default: false
+    },
+    throttle: {
+      type: Number,
+      default: 500 // 当throttle <= 0，无防暴力点击
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    type: {
+      type: String,
+      default: 'primary'
+    },
+    openType: String,
+    opacity: {
+      type: Boolean,
+      default: false
+    }
+  }
+
+  computed = {
+    stateClass() {
+      const classes = ['group', 'plain', 'disabled', 'opacity']
+      let stateClass = ''
+      classes.forEach(item => {
+        if (this[item] && (item != 'radius' || !this.group)) {
+          stateClass += `is-${item} `
+        }
+      })
+      return stateClass
+    }
+  }
+  
+  methods = {
+    async  __emit__(type, data = {}) {
+      console.log(data)
+      if (this.throttle > 0) {
+        await this.$throttle(this.throttle)
+      }
+      this.$cmlEmit(type, data)
+    }
+    
+  }
+
+}
+
+export default HButtonMixins
