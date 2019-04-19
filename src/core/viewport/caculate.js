e4a155cf40cb85f31f351572a19240d3650304e1
ChenXiaoSong12321 <448216252@qq.com>
2019-04-19 17:38:09 +0800

    add popup push at 2019.04.19 by ChenXiaoSong12321

---
+++ src/core/viewport/caculate.js
@@ -0,0 +1,36 @@
+import cml from 'chameleon-api'
+import defaultData from './defaultData'
+import getChannel from '../utils/channel'
+export default async function calculate() {
+  const data = JSON.parse(JSON.stringify(defaultData))
+  const system = await cml.getSystemInfo()
+  const channel = await getChannel()
+  data.channel = channel
+  data.viewportWidth = parseInt(system.viewportWidth)
+  data.viewportHeight = parseInt(system.viewportHeight)
+  // console.log(system, channel)
+  switch (channel) {
+    case 'HP_MALL':
+      if (system.extraParams.platform == 'devtools') {
+        data.capsuleHeight = 44
+      } else if (system.os == 'android') {
+        data.capsuleHeight = 48
+      }
+      data.statusBarHeight = system.extraParams.statusBarHeight
+      break
+    case 'HP_WECHAT':
+      data.capsuleHeight = 0
+      break
+    default:
+      break
+  }
+  if (data.viewportHeight - data.statusBarHeight > 750 && system.os != 'android') data.isAllScreen = true
+  data.titleHeight = data.capsuleHeight + data.statusBarHeight
+  if (data.statusBarHeight >= 44) {
+    data.isHighHead = true
+  }
+  data.headerHeight = data.statusBarHeight + data.capsuleHeight
+  // 全面屏 底部留空距离 34px
+  data.bottomHeight = 34
+  return data
+}
