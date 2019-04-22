d059375df7225abbaaab6e1deed0fb239ef36418
ChenXiaoSong12321 <448216252@qq.com>
2019-04-22 14:48:55 +0800

    solve wx: ---> c- push at 2019.04.22 by ChenXiaoSong12321

--- src/core/mixins/page/hupoApi.js
+++ src/core/mixins/page/hupoApi.js
@@ -6,34 +6,36 @@ const defaultOptions = options => typeof options === 'string' ? {
 } : options
 
 export default class HupoApi{
-  $alert(options) {
-    options = defaultOptions(options)
-    return Dialog({
-      title: options.title || '',
-      message: options.message || '',
-      openType: options.openType || '',
-      selector: '#h-dialog',
-      showCancelButton: false,
-      confirmButtonText: options.confirmButtonText || '好的',
-    })
-  }
-  $confirm(options) {
-    options = defaultOptions(options)
-    return Dialog({
-      title: options.title || '',
-      message: options.message || '',
-      html: options.html || '',
-      openType: options.openType || '',
-      selector: '#h-dialog',
-      showCancelButton: options.showCancelButton !== undefined ? options.showCancelButton : true,
-      confirmButtonText: options.confirmButtonText || '确定',
-      cancelButtonText: options.cancelButtonText || '取消'
-    })
-  }
-  $loading(options){
-    console.log('loading')
-  }
-  $toast(options){
-    console.log('toast')
+  methods = {
+    $alert(options) {
+      options = defaultOptions(options)
+      return Dialog({
+        title: options.title || '',
+        message: options.message || '',
+        openType: options.openType || '',
+        selector: '#h-dialog',
+        showCancelButton: false,
+        confirmButtonText: options.confirmButtonText || '好的',
+      })
+    },
+    $confirm(options) {
+      options = defaultOptions(options)
+      return Dialog({
+        title: options.title || '',
+        message: options.message || '',
+        html: options.html || '',
+        openType: options.openType || '',
+        selector: '#h-dialog',
+        showCancelButton: options.showCancelButton !== undefined ? options.showCancelButton : true,
+        confirmButtonText: options.confirmButtonText || '确定',
+        cancelButtonText: options.cancelButtonText || '取消'
+      })
+    },
+    $loading(options){
+      console.log('loading')
+    },
+    $toast(options){
+      console.log('toast')
+    }
   }
 }
