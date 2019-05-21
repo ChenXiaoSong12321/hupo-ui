290198368e031a9ae808fc14825b6f10843c467b
卢逸平 <372254032@qq.com>
2019-05-21 16:22:39 +0800

    解决冲突

--- src/core/mixins/page/pullRefresh.js
+++ src/core/mixins/page/pullRefresh.js
@@ -11,8 +11,26 @@ export default class PullRefresh {
   methods = {
     $getScrollComponents() {
       const scrolls = difference.selector(this, '.cml-h-scroll')
-      if (scrolls.length == 0) {
-        console.error('请给h-scroll组件添加ref="h-scroll"属性')
+      if(scrolls.length == 0){
+        let result = []
+        const find = ctxs => {
+          let subComponents = []
+          ctxs.forEach(ctx => {
+            subComponents = subComponents.concat(difference.selector(ctx, '.cml-view'))
+          })
+          subComponents.forEach(item => {
+            const subScrolls = difference.selector(item, '.cml-h-scroll')
+            if(subScrolls.length > 0){
+              result = subScrolls
+            }
+          })
+          if(subComponents.length > 0){
+            find(subComponents)
+          }
+        }
+        find([this])
+        if(result.length == 0)console.warn('未能找到h-scroll组件')
+        return result
       }
       return scrolls
     }
