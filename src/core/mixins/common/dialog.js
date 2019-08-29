cf2a5605683c6e25f4cd6e820a5ed532b40e0e12
Jerry12138 <448216252@qq.com>
2019-08-29 16:52:49 +0800

    fix toast

--- src/core/mixins/common/dialog.js
+++ src/core/mixins/common/dialog.js
@@ -1,40 +1,41 @@
 import { dialogOptions } from '../../../components/h-dialog/dialog'
-const parseOptions = options => typeof options === 'string' ? {
-  message: options
-} : options
+
+const parseOptions = options => typeof options === 'string' ? { message: options } : options
+const hanleDialogOptions = (options,typeOptions) => {
+  options = parseOptions(options)
+  return dialogOptions({
+    ...options,
+    ...typeOptions,
+    title: options.title || '',
+    message: options.message || '',
+    openType: options.openType || '',
+  })
+}
 
 export default {
   methods: {
-    _alert(options) {
-      return new Promise((resolve,reject)=>{
-        options = parseOptions(options)
-        options = dialogOptions({
-          title: options.title || '',
-          message: options.message || '',
-          openType: options.openType || '',
-          showCancelButton: false,
-          confirmButtonText: options.confirmButtonText || '好的',
-          promise:{
-            resolve, reject
-          }
-        })
-        this._broadcast('h-dialog', 'setDialogOptions', options)
+    async _alert(options) {
+      await this.__handleDialogBroadcast(options, {
+        showCancelButton: false,
+        confirmButtonText: options.confirmButtonText || '好的',
       })
     },
-    _confirm(options) {
+    async _confirm(options) {
+      await this.__handleDialogBroadcast(options, {
+        showCancelButton: options.showCancelButton !== undefined ? options.showCancelButton : true,
+        confirmButtonText: options.confirmButtonText || '确定',
+        cancelButtonText: options.cancelButtonText || '取消',
+      })
+    },
+    __handleDialogBroadcast(options, typeOptions){
       return new Promise((resolve,reject)=>{
-        options = parseOptions(options)
-        options = dialogOptions({
-          title: options.title || '',
-          message: options.message || '',
-          openType: options.openType || '',
-          showCancelButton: options.showCancelButton !== undefined ? options.showCancelButton : true,
-          confirmButtonText: options.confirmButtonText || '确定',
-          cancelButtonText: options.cancelButtonText || '取消',
-          promise:{
+        options = hanleDialogOptions(options, typeOptions)
+        options = {
+          ...options,
+          promise: {
             resolve, reject
           }
-        })
+        }
         this._broadcast('h-dialog', 'setDialogOptions', options)
       })
     }
