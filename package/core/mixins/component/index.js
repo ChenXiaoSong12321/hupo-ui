0c26676ce9c9fb59ddb70851dc16f16a1a48d547
卢逸平 <372254032@qq.com>
2019-09-11 14:50:36 +0800

    publish

--- package/core/mixins/component/index.js
+++
@@ -1,7 +0,0 @@
-import Goto from '../common/goto'
-import HDialog from '../common/dialog'
-import HTool from '../common/tools'
-import HToast from '../common/toast'
-
-import {componentBaseTreeMixin, componentEventMixin, timer} from '@hupo/core'
-export default [componentBaseTreeMixin, componentEventMixin, Goto, HDialog, HTool, HToast, timer]
