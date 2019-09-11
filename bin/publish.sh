a5751c1d366f6d6632ac158ea26b5f8c99a1c5aa
卢逸平 <372254032@qq.com>
2019-09-11 10:32:03 +0800

    modify sh

--- bin/publish.sh
+++ bin/publish.sh
@@ -4,5 +4,5 @@ rm -rf ./packages/hupo-cml-ui/components
 rm -rf ./packages/hupo-cml-ui/assets
 rm -rf ./packages/hupo-cml-ui/core
 cp -R ./src/components ./packages/hupo-cml-ui
-cp -R  ./src/assets ./packages/hupo-cml-ui
-cp -R  ./src/core ./packages/hupo-cml-ui
+cp -R ./src/assets ./packages/hupo-cml-ui
+cp -R ./src/core ./packages/hupo-cml-ui
