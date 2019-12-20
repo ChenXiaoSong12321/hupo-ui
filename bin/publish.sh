6648bdd6f4c9ff52928dc9c68ea260de57d554f8
卢逸平 <372254032@qq.com>
2019-12-20 18:01:56 +0800

    cml-ui >> ui

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -rf ./packages/hupo-cml-ui/components
-rm -rf ./packages/hupo-cml-ui/core
-cp -R ./src/components ./packages/hupo-cml-ui
-cp -R ./src/core ./packages/hupo-cml-ui
+rm -rf ./packages/hupo-ui/components
+rm -rf ./packages/hupo-ui/core
+cp -R ./src/components ./packages/hupo-ui
+cp -R ./src/core ./packages/hupo-ui
--- packages/hupo-cml-ui/h-rich-text/recursion.js
+++
@@ -1,5 +0,0 @@
-export const beforeCreate = ctx => {
-  // #ifdef H5
-  ctx.$options.components.hRichTextParseBase = require('./components/base/h-rich-text-parse-base').default
-  // #endif
-}
